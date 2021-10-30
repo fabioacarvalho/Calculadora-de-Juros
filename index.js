90236499d27786cd646fc645e11812c2cc03dc88
Fábio Carvalho <fabioxcarvalho@outlook.com>
2021-10-30 11:10:42 -0300

    u

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+let ap = document.getElementsById('aporte')
+let ju = document.getElementsById('rendimento')
+let te = document.getElementsById('tempo')
+let ti = document.getElementsById('title')
 
-let aporte = document.getElementsByName('aporte')
-let juros = document.getElementsByName('rendimento')
-let tempo = document.getElementsByName('tempo')
-let total = ''
+let aporte = Number(ap.value)
+let juros = Number(ju)
+let tempo = Number(te)
+let title = Number(ti)
 
 function calcular(aporte, juros, tempo) {
-    prazo = (tempo/12) - 1
-    total = (aporte * (1 + (juros/100)^prazo) ) / (juros / 100)
+    /* prazo = (tempo/12) - 1
+    totalf = (aporte * (1 + (juros/100)^prazo) ) / (juros / 100) */
+    totalf = 2 * 2
+    alert(aporte)
+
 }
+
